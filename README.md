b0d8a1c8f450239022ba1413863fc100f5022c76
Denys <constandvar@gmail.com>
2020-11-15 23:25:47 +0200

    md, future endpoint

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-genesis
+# genesis
+
+### Installation
+
+Clone the repository to your machine.
+
+#### Start
+```sh
+$ PORT=8080 go run main.go
+```
+#### Test
+```sh
+$ go test ./...
+```
--- api/payment.go
+++ api/payment.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"fmt"
 	"github.com/JIakki/genesis/api/controllers"
 	"github.com/JIakki/genesis/db"
 	"github.com/JIakki/genesis/modules/payment"
@@ -13,7 +12,6 @@ func NewPaymentHandler(mux *http.ServeMux, database *db.DB) *http.ServeMux {
 	repo := payment.NewRepository(payment.NewMapper(database))
 	mux.HandleFunc("/payment/buttons", func(w http.ResponseWriter, r *http.Request) {
 		ctrl := controllers.NewGetPaymentButtonsCtrl(repo)
-		fmt.Println(ctrl.StartedAt)
 
 		switch r.Method {
 		case http.MethodPost:
@@ -21,6 +19,17 @@ func NewPaymentHandler(mux *http.ServeMux, database *db.DB) *http.ServeMux {
 		default:
 			ctrl.NotFound(w, r, "404")
 		}
+	})
+
+	mux.HandleFunc("/payment/callback/{service}", func(w http.ResponseWriter, r *http.Request) {
+		// UseAuthMiddleWare(w, r)
+		// controllers.NewPaymentCallbackCtrl(repo)
+		// switch r.Method {
+		// case http.MethodPost:
+		// ctrl.Execute(w, r)
+		// default:
+		// ctrl.NotFound(w, r, "404")
+		// }
 	})
 
 	return mux
